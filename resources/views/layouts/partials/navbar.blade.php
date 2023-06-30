461abebb2800868ae402869c0b58bbb6b1179a2c
BryanGuapulema <bryan19012003@gmail.com>
2023-06-29 23:16:48 -0500

    crud_root_y_usuarios

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -5,6 +5,7 @@
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
       </a>
 
+      @auth  
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
         <li><a href="#" class="nav-link px-2 text-white">Perfil</a></li> 
@@ -12,15 +13,33 @@
             <li><a href="/users" class="nav-link px-2 text-white">Administrar</a></li> 
         @endif     
       </ul>
+        
+                    
+        <div class="btn-group">
+          <button type="button" class="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          {{auth()->user()->name}}
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">{{auth()->user()->name}}</a></li>
+            <li><a class="dropdown-item" href="#">{{auth()->user()->username}}</a></li>
+            <li><a class="dropdown-item" href="#">Editar Perfil</a></li>
+            <li><hr class="dropdown-divider">
+              <a href="{{url('users/' .auth()->user()->id.'/edit')}}" class="btn btn-warning btn-sm">
+                  Editar cuenta
+              </a>
+            </li>       
+            <li><a class="dropdown-item btn btn-danger btn-sm" href="#">     
+              <form action="{{url('users/'. auth()->user()->id)}}" method="post">
+                  @method('DELETE')
+                  @csrf                  
+                  <button type="submit" class="btn btn-danger btn-sm">Eliminar cuenta</button>
+              </form>
+            </a></li>
+          </ul>
+        </div>
 
-      <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-      </form>
         
-      @auth
-        {{auth()->user()->name}}
-        <div class="text-end px-2">
-        
+        <div class="text-end px-2">        
           <a href="/logout" class="btn btn-outline-light me-2">Logout</a>
 
         </div>
