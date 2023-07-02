a188da363fe1bec636d0c7d00dd37332087ba640
BryanGuapulema <bryan19012003@gmail.com>
2023-07-02 14:24:05 -0500

    validation username change to roles

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -9,7 +9,7 @@
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
         <li><a href="#" class="nav-link px-2 text-white">Perfil</a></li> 
-        @if(auth()->user()->username==='root')
+        @if(auth()->user()->role()->first()->name ==='Administrador')
             <li><a href="/users" class="nav-link px-2 text-white">Administrar</a></li> 
         @endif     
       </ul>
