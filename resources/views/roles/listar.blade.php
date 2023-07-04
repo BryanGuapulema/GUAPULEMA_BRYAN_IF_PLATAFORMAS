a95d74b19eee0edc8fac4da8d8256eaa89bdf27c
BryanGuapulema <bryan19012003@gmail.com>
2023-07-03 22:25:09 -0500

    cambio de diseño de botones

--- resources/views/roles/listar.blade.php
+++ resources/views/roles/listar.blade.php
@@ -19,8 +19,8 @@
                     <th>Nombre</th>
                     <th>Institucion</th>                               
                     <th>Estado</th>
-                    <th></th>
-                    <th></th>
+                    <th></th>  
+                    <th></th>              
                 </tr>
             </thead>
 
@@ -30,18 +30,31 @@
                     <td>{{$role->id}}</td>
                     <td>{{$role->name}}</td>
                     <td>{{$role->institution}}</td>
-                    <td>{{$role->state}}</td>                               
+                    <td>{{$role->state}}</td>
+                    <td></td>
                     <td>
-                        <a href="{{url('roles/' .$role->id .'/edit')}}" class="btn btn-warning btn-sm">
-                            Editar
-                        </a>
-                    </td>
-                    <td>
-                        <form action="{{url('roles/'. $role->id)}}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                        </form>
+                        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <form>
+                                <a href="{{url('roles/' .$role->id .'/edit')}}" class="btn btn-success btn-sm">
+                                    Editar
+                                </a>
+                            </form>
+
+<!--
+
+                            <form action="{{url('roles/'. $role->id)}}" method="post">
+                                @method('PUT')
+                                @csrf
+                                <button type="submit" class="btn btn-warning btn-sm">Cambiar Estado</button>
+                            </form>
+-->                    
+
+                            <form action="{{url('roles/'. $role->id)}}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
                 @endforeach
